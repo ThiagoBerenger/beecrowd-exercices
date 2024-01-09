e656332a28f5a28de9d3bf27e9f0805e6f28e1fe
Thiago <thberenger27@gmail.com>
2024-01-08 23:41:22 -0300

    new 1012.js

--- 0-iniciante/1012.js
+++ 0-iniciante/1012.js
@@ -1,7 +1,27 @@
+const { type } = require('os');
 var input = require('fs').readFileSync('./dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-// 3.0 4.0 5.2
+const [num1, num2, num3] = lines.shift().split(' ')
 
+// conversão
 
-console.log(lines)
+const a = parseFloat(num1)
+const b = parseFloat(num2)
+const c = parseFloat(num3)
+
+// formulas ---
+
+const areaTrianguloRetangulo = (a * c) / 2
+const areaCirculo = 3.14159 * (Math.pow(c, 2))
+const areaTrapezio = ((a + b) / 2) * c
+const areaQuadrado = b * b
+const areaRetangulo = a * b
+
+// impressão
+
+console.log(`TRIANGULO: ${areaTrianguloRetangulo.toFixed(3)}`)
+console.log(`CIRCULO: ${areaCirculo.toFixed(3)}`)
+console.log(`TRAPEZIO: ${areaTrapezio.toFixed(3)}`)
+console.log(`QUADRADO: ${areaQuadrado.toFixed(3)}`)
+console.log(`RETANGULO: ${areaRetangulo.toFixed(3)}`)
