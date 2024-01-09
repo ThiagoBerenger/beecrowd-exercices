d2154b186c57e6ca97c5829be26631e910b542d3
Thiago <thberenger27@gmail.com>
2024-01-08 23:22:15 -0300

    test att

--- 0-iniciante/1004.js
+++
@@ -1,7 +0,0 @@
-var input = require('fs').readFileSync('./dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-const a = parseInt(lines.shift())
-const b = parseInt(lines.shift())
-
-console.log(`PROD = ${a * b}`)
--- 0-iniciante/1011.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('./dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-const r = lines.shift()
-const volume = (4/3) * 3.14159 * (Math.pow(r, 3))
-
-console.log(`VOLUME = ${volume.toFixed(3)}`)
-
